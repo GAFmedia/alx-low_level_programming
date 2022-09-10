1ede2b030c0c7dd29fcbc902cca59d4c68ab7138
GAFmedia <adefioyeafolake60@gmail.comgit config --global user.name GAFmedia>
2022-09-09 18:05:59 -0700

    h

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,14 +2,14 @@
 #include <ctype.h>
 
 /**
-     * main - program that prints the alphabet in lowercase, followed by a new line
-     * You can only use the putchar
+     * main - prints the alphabet in lowrcase
      * Return: 0
      */
 
 int main(void)
 {
 	int l = 'a';
+
 	while (l <= 'z')
 	{
 		putchar(l);
