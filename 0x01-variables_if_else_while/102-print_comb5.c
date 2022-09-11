76075b56b45b1746bb9a06e455c0d98e35f66c6e
GAFmedia <adefioyeafolake60@gmail.comgit config --global user.name GAFmedia>
2022-09-11 16:03:41 -0700

    variable tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,27 +10,30 @@
 
 int main(void)
 {
-	int i' j:
+	int n1 = 48;
+	int a = 0;
+	int b;
+	int com = 44;
 
-		for (i = 0; i < 100; i++)
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
 		{
-			for (j = 0; j < 100; j++)
+			putchar((a / 10) + n1);
+			putchar((a % 10 + n1);
+			putchar(32);
+			putchar((b / 10) + n1);
+			putchar((b % 10) + n1);
+			if (a != 98 || b != 99)
 			{
-				if (i < j)
-				{
-					putchar((i / 10) + 48);
-					putchar((i % 10) + 48);
-					putchar(' ');
-					putchar((j /10) + 48);
-					putchar((j % 10) + 48);
-					if (i != 98 || j !=99)
-					{
-						putchar(com);
-						putchar(32);
-					}
-				}
+				putchar(com);
+				putchar(32);
 			}
+			b += 1;
 		}
+		a += 1;
+	}
 	putchar('\n');
 	return (0);
 }
