13d2560b62cd4eabe92f50c1af473b92508877f0
GAFmedia <adefioyeafolake60@gmail.comgit config --global user.name GAFmedia>
2022-09-13 18:21:37 -0700

    wahala

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower -This program check
+ * if a character is a lowercase
+ * from the English alphabet
+ * @c: input parameter
+ * Return: 1 for lowercase, 0 if it's not lowercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+}
